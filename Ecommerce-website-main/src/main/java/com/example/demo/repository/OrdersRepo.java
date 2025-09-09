0d84a36bf5dbfd4bb1d7ac06d4439a3153aa560f
Ashton <ashutosh20.acharya@gmail.com>
2025-09-09 11:33:06 +0530

    Delete Ecommerce-website-main directory

--- Ecommerce-website-main/src/main/java/com/example/demo/repository/OrdersRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Orders;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrdersRepo extends JpaRepository<Orders, Integer> {
-
-}
