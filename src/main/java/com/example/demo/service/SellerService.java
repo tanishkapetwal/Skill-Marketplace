5322aafd5cfde63c584a4121a018bfdb31e6e3fe
tanishkapetwal <tanishka.petwal@argusoft.in>
2025-09-10 10:27:04 +0530

    Update SellerService.java

--- src/main/java/com/example/demo/service/SellerService.java
+++ src/main/java/com/example/demo/service/SellerService.java
@@ -1,7 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.CreateListingDTO;
 import com.example.demo.model.Seller;
+import com.example.demo.model.SkillsListing;
 import com.example.demo.repository.SellerRepo;
+import com.example.demo.repository.SkillsListingRepo;
+import com.example.demo.repository.SkillsRepo;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,20 +17,38 @@ import java.util.List;
 public class SellerService {
 
     @Autowired
-    private SellerRepo repo;
+    private ModelMapper modelmapper;
+    @Autowired
+    private SkillsRepo skillsRepo;
+    @Autowired
+    private SellerRepo sellerRepo;
+    @Autowired
+    private SkillsListingRepo skillsListingRepo;
+
     public List<Seller> getSeller() {
-        return repo.findAll();
+        return sellerRepo.findAll();
     }
 
     public List<Seller> getSellerById(Integer id) {
-        return repo.findAllById(Collections.singleton(id));
+        return sellerRepo.findAllById(Collections.singleton(id));
     }
 
     public void addSeller(Seller seller) {
-        repo.save(seller);
+        sellerRepo.save(seller);
     }
 
     public void deleteSeller(int id) {
-        repo.deleteById(id);
+        sellerRepo.deleteById(id);
     }
+    public void addSkillsListing(int skillId, CreateListingDTO createListingDTO, int sellerId) {
+        SkillsListing skillsListing=new SkillsListing();
+
+        skillsListing = modelmapper.map(createListingDTO, SkillsListing.class);
+        skillsListing.setSkills(skillsRepo.findById(skillId).orElseThrow(RuntimeException::new));
+        skillsListing.setSeller(sellerRepo.findById(sellerId).orElseThrow(RuntimeException::new));
+        skillsListing.setTitle(createListingDTO.getTitle());
+        skillsListing.setDescription(createListingDTO.getDescription());
+
+        skillsListingRepo.save(skillsListing);}
+
 }
