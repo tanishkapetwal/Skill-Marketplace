033cb08e77d808ff2cde618f302dead88fcf961b
Ashutosh <ashutosh.acharya@argusoft.com>
2025-09-13 14:49:11 +0530

    bugs fixed

--- src/main/java/com/example/demo/repository/OrdersRepo.java
+++ src/main/java/com/example/demo/repository/OrdersRepo.java
@@ -12,10 +12,10 @@ import java.util.Optional;
 public interface OrdersRepo extends JpaRepository<Orders, Integer> {
 
     @Query("SELECT AVG(o.orderRating) FROM Orders o WHERE o.skillslisting.id = :listingId AND o.orderRating > 0")
-    Double findAvgByListing(@Param("listingId") Integer listingId);
+    Double findAvgByListing(@Param("listingId") int listingId);
 
     @Query("SELECT AVG(o.orderRating) FROM Orders o JOIN o.skillslisting l WHERE l.seller.id = :sellerId AND o.orderRating > 0")
-    Double findAvgBySeller(@Param("sellerId") Integer sellerId);
+    Double findAvgBySeller(@Param("sellerId") int sellerId);
 
     List<Orders> findBySkillslisting_SellerId(int sellerId);
 }
