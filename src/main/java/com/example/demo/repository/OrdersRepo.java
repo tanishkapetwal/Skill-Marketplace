efdae76b708e9d7b35573f95a9f9d34906ae86bc
Ashutosh <ashutosh.acharya@argusoft.com>
2025-09-13 18:53:26 +0530

    Admin show seller and customer list

--- src/main/java/com/example/demo/repository/OrdersRepo.java
+++ src/main/java/com/example/demo/repository/OrdersRepo.java
@@ -1,14 +1,13 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Orders;
-import com.example.demo.model.SkillsListing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
-
+@Repository
 public interface OrdersRepo extends JpaRepository<Orders, Integer> {
 
     @Query("SELECT AVG(o.orderRating) FROM Orders o WHERE o.skillslisting.id = :listingId AND o.orderRating > 0")
