863a6f34ab43e6a63f44a882cb6f7a99abadf574
tanishkapetwal <tanishka.petwal@argusoft.in>
2025-09-29 12:02:23 +0530

    Update SellerController.java

--- src/main/java/com/example/demo/controller/SellerController.java
+++ src/main/java/com/example/demo/controller/SellerController.java
@@ -29,7 +29,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
+// @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/seller")
 @RequiredArgsConstructor
