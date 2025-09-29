6bc879478f4e3d204ae606d8204631e1a8449496
tanishkapetwal <tanishka.petwal@argusoft.in>
2025-09-29 12:01:56 +0530

    Update CustomerController.java

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -33,7 +33,7 @@ import java.util.Map;
 import static java.time.LocalDate.*;
 
 
-@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+// @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 @RestController
 @RequestMapping("/customer")
 @RequiredArgsConstructor
